b88c7dfb116fd667f78c60e6da8f4cfdfbb72dda
Antonio Malara <antonio.malara@booking.com>
2015-04-12 06:12:07 +0200

    rewriting with linked lists. also, idiotic variable names

--- js/data.js
+++ js/data.js
@@ -81,6 +81,62 @@ var linetypes = {
 
 // ------ //
 
+function ListItem(item, prev, next) {
+    this.item = item;
+    this.prev = prev;
+    this.next = next;
+}
+
+function list_append(list, item) {
+    if (!list) {
+        return new ListItem(item, null, null);
+    }
+    else {
+        var list_item = new ListItem(item, list, null);
+
+        if (list.next) {
+            list.next.prev = list_item;
+            list_item.next = list.next;
+        }
+
+        list.next = list_item;
+
+        return list_item;
+    }
+}
+
+function list_replace(list, item) {
+    var x = new ListItem(item, list.prev, list.next);
+    list.prev.next = x;
+    list.next.prev = x;
+    return x;
+}
+
+function list_foreach(list, f) {
+    var it = list;
+    while (it) {
+        f(it);
+        it = it.next;
+    }
+}
+
+function list_replace(list, item) {
+    var x = new ListItem(item, list.prev, list.next);
+    list.prev.next = x;
+    list.next.prev = x;
+
+    return x;
+}
+
+function list_delete(list) {
+    list.next.prev = list.prev;
+    list.prev.next = list.next;
+
+    return list.next;
+}
+
+// ------ //
+
 var fanculo;
 
 function changeline(line, select) {
@@ -91,15 +147,22 @@ function Lines(rom, element) {
     fanculo = this;
 
     this.rom     = rom;
-    this.lines   = [];
     this.element = element;
 
+    element.appendChild(document.createElement('div'));
+
+    this.lines_list = null;
+
+    var iterator   = null;
     for (var i = 0; i < rom.size(); i++) {
-        this.lines.push(linetypes.uint8.create(
+        var line = linetypes.uint8.create(
             i,
             rom.base + i,
             rom
-        ));
+        );
+
+        iterator = list_append(iterator, line);
+        if (!this.lines_list) this.lines_list = iterator;
     }
 
     this.load();
@@ -113,8 +176,17 @@ Lines.prototype.load = function() {
 
     xmlhttp.onreadystatechange=function() {
         if (xmlhttp.readyState == 4) {
-            var json = xmlhttp.responseText;
-            the_this.lines = JSON.parse(json);
+            var json  = xmlhttp.responseText;
+            var lines = JSON.parse(json);
+            var it    = null;
+
+            the_this.lines_list = null;
+
+            for (var i in lines) {
+                it = list_append(it, lines[i]);
+                if (!the_this.lines_list) the_this.lines_list = it;
+            }
+
             the_this.render();
         }
     }
@@ -125,87 +197,161 @@ Lines.prototype.load = function() {
 
 Lines.prototype.render = function() {
     var str = [];
-    for (var i in this.lines) {
-        var line = this.lines[i];
-        line.line = i;
-        str.push(render_line(line));
-    }
+    
+    var append_here = document.createElement('div');
+    this.element.replaceChild(append_here, this.element.firstChild);
 
-    this.element.innerHTML = str.join('');
+    var this_lines = this;
 
-    var content = JSON.stringify(this.lines);
-    document.getElementById('json').value = content;
+    list_foreach(
+        this.lines_list,
+        function (line_item) {
+            line_item.item.element = render_lineitem(line_item, this_lines);
+            append_here.appendChild(line_item.item.element);
+        }
+    );
+
+    //var content = JSON.stringify(this.lines);
+    //document.getElementById('json').value = content;
 }
 
-Lines.prototype.fix_lines = function(linenr, newline) {
+Lines.prototype.fix_lines = function(line_item, newline) {
+    var target_size   = newline.size;
+    var deleted_size  = 0;
+    var deleted_lines = 0;
+
+    var deleting      = line_item;
+
+    while (deleted_size < target_size) {
+        deleted_size += deleting.item.size;
+        deleting = list_delete(deleting);
+        deleted_lines++;
+    }
+
+    var new_lineitem  = list_append(line_item.prev, newline);
+    new_lineitem.item.element = render_lineitem(new_lineitem, this);
+
+    var padding_addr  = newline.addr + newline.size;
+    var padding_bytes = deleted_size - target_size;
+    var pad_item      = new_lineitem;
+
+    line_item.item.element.parentNode.insertBefore(new_lineitem.item.element, line_item.item.element);
+
+    for (var i = 0; i < padding_bytes; i++) {
+        var pad = linetypes.uint8.create(null, padding_addr + i, this.rom)
+        pad_item = list_append(pad_item, pad);
+        pad_item.item.element = render_lineitem(pad_item, this);
+        line_item.item.element.parentNode.insertBefore(pad_item.item.element, line_item.item.element);
+    }
+
+
+    deleting = line_item.item.element;
+    for (var i = 0; i < deleted_lines; i++) {
+        var nextDeleting = deleting.nextSibling;
+        deleting.parentNode.removeChild(deleting);
+        deleting = nextDeleting;
+    }
+}
+
+Lines.prototype.fix_lines2 = function(line_item, newline) {
     var target_size     = newline.size;
     var to_delete_size  = 0;
     var to_delete_lines = 0;
-    
+
+    var i = line_item;
     while (to_delete_size < target_size) {
-        to_delete_size += this.lines[linenr + to_delete_lines].size;
+        to_delete_size += i.item.size;
         to_delete_lines++;
+        i = i.next;
     }
+
+    to_delete_lines--;
 
     var padding = to_delete_size - target_size;
 
-    var spliceparams = [
-        linenr,
-        to_delete_lines,
-        newline
-    ];
+    // --- //
 
-    var padding_addr_start = newline.addr + newline.size;
-    for (var i = 0; i < padding; i++) {
-        spliceparams.push(linetypes.uint8.create(linenr + i + 1, padding_addr_start + i, this.rom));
+    var new_lineitem = list_replace(line_item, newline);    
+    new_lineitem.item.element = render_lineitem(new_lineitem, this);
+
+    line_item.item.element.parentNode.replaceChild(new_lineitem.item.element, line_item.item.element);
+
+    var to_delete_item = new_lineitem.next;
+    for (var i = 0; i < to_delete_lines; i++) {
+        to_delete_item.item.element.parentNode.removeChild(to_delete_item.item.element);
+        to_delete_item = list_delete(to_delete_item);
     }
 
-    Array.prototype.splice.apply(this.lines, spliceparams);
-    this.render();
+    var padding_addr_start = newline.addr + newline.size;
+    var pad_item   = new_lineitem;
+    var before_pad = new_lineitem.next.item.element;
+
+    for (var i = 0; i < padding; i++) {
+        before_pad.parentNode.insertBefore(pad_item.item.element, before_pad);
+    }
+
+    pad_item.next = line_item.next;
 }
 
-Lines.prototype.convert = function(linenr, newtype) {
-    var line = this.lines[linenr];
+Lines.prototype.convert = function(line_item, newtype) {
+    var line = line_item.item;
+
     if (line.type == newtype)
         return;
 
     if (newtype == 'string') {
-        this.to_string(linenr);
+        this.to_string(line_item);
         return;
     }
 
-    var newline = linetypes[newtype].create(linenr, line.addr, this.rom);
-    this.fix_lines(linenr, newline);
+    var newline = linetypes[newtype].create(null, line.addr, this.rom);
+    this.fix_lines(line_item, newline);
 }
 
-Lines.prototype.to_string = function(linenr) {
-    var oldline = this.lines[linenr];
+Lines.prototype.to_string = function(line_item) {
+    var oldline = line_item.item;
 
-    if (this.lines[linenr - 1].type == 'string') {
-        var oldstring = this.lines[linenr - 1];
-        var newstring = linetypes.string.create(linenr - 1, oldstring.addr, this.rom, oldstring.size + 1);
-        this.fix_lines(linenr - 1, newstring);
+    if (line_item.prev.item.type == 'string') {
+        var oldstring = line_item.prev.item;
+        var newstring = linetypes.string.create(null, oldstring.addr, this.rom, oldstring.size + 1);
+        this.fix_lines(line_item.prev, newstring);
     }
     else {
-        var newline = linetypes.string.create(linenr, oldline.addr, this.rom, 1);
-        this.fix_lines(linenr, newline)
+        var newline = linetypes.string.create(null, oldline.addr, this.rom, 1);
+        this.fix_lines(line_item, newline)
     }
 }
+
+
+function render_lineitem(line_item, lines) {
+    var element = document.createElement('div');
+    element.innerHTML = render_line(line_item.item);
+
+    var select = element.getElementsByTagName('select')[0];
+    select.onchange = function () {
+        lines.convert(line_item, this.value);
+    }
+
+    return element;
+}
+
 
 __conversions = Object.keys(linetypes);
 __conversions.sort();
+__conversions_options = '';
+for (var c in __conversions) 
+    __conversions_options += "<option>" + __conversions[c] + "</option>";
 
 function render_line(line) {
-    all = sprintf("%04X  -  ", line.addr);
-    all += '<select onchange="javascript:changeline(' + line.line + ', this)"><option></option>';
+    var addr = sprintf("%04X  -  ", line.addr);
+    var html = linetypes[line.type].to_html(line);
 
-    if (!__conversions) {
-    }
+    var all = ['<pre>', addr];
+    all.push('<select><option></option>');
+    all.push(__conversions_options);
+    all.push('</select>  -  ');
+    all.push(html);
+    all.push('</pre>\n');
 
-    for (var c in __conversions) 
-        all += "<option>" + __conversions[c] + "</option>";
-
-    all += '</select>  -  ' + linetypes[line.type].to_html(line) + '\n';
-
-    return all;
+    return all.join('');
 }
